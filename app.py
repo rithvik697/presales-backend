63ba19113b0e70d1b926df6201420bff7b614283
rithvik697 <rithvikmaragoni9@gmail.com>
2026-02-06 16:30:33 +0530

    Added login service and controllers

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask_cors import CORS
 from controllers.project_controller import project_bp
 from controllers.call_logs_controller import call_logs_bp
 from controllers.user_controller import user_controller_bp 
+from controllers.auth_controller import auth_controller_bp
 
 app = Flask(__name__)
 CORS(app)
 
 # Register Blueprints
+app.register_blueprint(auth_controller_bp, url_prefix='/api')
 app.register_blueprint(project_bp, url_prefix="/api")
 app.register_blueprint(call_logs_bp, url_prefix="/api/calls")
 app.register_blueprint(user_controller_bp, url_prefix="/api")  
