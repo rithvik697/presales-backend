9ff9e8d4193b50ecac561641bada3474b7114257
rithvik697 <rithvikmaragoni9@gmail.com>
2026-03-06 16:56:35 +0530

    Added forgot pass and email integration

--- services/email_service.py
+++ services/email_service.py
@@ -31,3 +31,33 @@ CRM Team
     server.login(SMTP_EMAIL, SMTP_PASSWORD)
     server.send_message(msg)
     server.quit()
+
+def send_reset_email(email, reset_link):
+
+    subject = "Reset Your Password"
+
+    body = f"""
+    Hello,
+
+    Click the link below to reset your password:
+
+    {reset_link}
+
+    This link will expire in 15 minutes.
+
+    If you did not request a password reset, please ignore this email.
+
+    Regards,
+    CRM Team
+    """
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = SMTP_EMAIL
+    msg["To"] = email
+
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    server.starttls()
+    server.login(SMTP_EMAIL, SMTP_PASSWORD)
+    server.send_message(msg)
+    server.quit()
